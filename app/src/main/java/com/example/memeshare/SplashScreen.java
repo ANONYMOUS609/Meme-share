ed5bc29beb4110862dd290af435dee49637b9a69
admin <mankum08071998@gmail.com>
2020-10-05 05:25:38 +0530

    splash screen added

---
+++ app/src/main/java/com/example/memeshare/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.memeshare;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, 2000);
+    }
+}
