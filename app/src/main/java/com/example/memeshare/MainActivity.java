4bdbee345e681533c58ce316c9f312698ad7023a
admin <mankum08071998@gmail.com>
2020-10-11 19:57:28 +0530

    loading screen improved

--- app/src/main/java/com/example/memeshare/MainActivity.java
+++ app/src/main/java/com/example/memeshare/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.memeshare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private ImageView meme_image;
     private TextView caption;
     private ProgressBar progressBar;
+    public static String ImgUrl = "";
+    public static String CapUrl = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +33,14 @@ public class MainActivity extends AppCompatActivity {
         caption = findViewById(R.id.caption);
         meme_image = findViewById(R.id.meme);
         progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
 
         fetchMeme();
-        progressBar.setVisibility(View.VISIBLE);
 
     }
 
     public void refresh_btn(View view) {
         fetchMeme();
-        progressBar.setVisibility(View.VISIBLE);
     }
 
     public void fetchMeme(){
@@ -49,21 +51,35 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<Meme> call, Response<Meme> response) {
 
                 if(!response.isSuccessful()){
-                    Toast.makeText(MainActivity.this, "Ruko zara thoda sabr kro", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "404 not found", Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
                     call.cancel();
                 }
                     Meme meme = response.body();
-                    progressBar.setVisibility(View.VISIBLE);
                 assert meme != null;
                 caption.setText(meme.getTitle());
+                ImgUrl = meme.getUrl();
+                CapUrl = meme.getTitle();
+
                     Glide.with(getApplicationContext()).load(meme.getUrl()).into(meme_image);
 
             }
 
             @Override
             public void onFailure(Call<Meme> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Sorry Babu", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+                meme_image.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    public void share(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, ImgUrl);
+        intent.putExtra(Intent.EXTRA_SUBJECT, CapUrl +" meme");
+        startActivity(Intent.createChooser(intent, "Share Using"));
+
     }
 }
