a45741bae9967f53ac67b7dbcb876f8607a014e3
admin <mankum08071998@gmail.com>
2020-10-15 22:42:36 +0530

    Image Sharing option updated

--- app/src/main/java/com/example/memeshare/MainActivity.java
+++ app/src/main/java/com/example/memeshare/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.memeshare;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -9,11 +15,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
-import com.airbnb.lottie.LottieAnimationView;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.example.memeshare.json.ApiHandler;
 import com.example.memeshare.json.Meme;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -76,11 +89,53 @@ public class MainActivity extends AppCompatActivity {
 
     public void share(View view) {
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, ImgUrl);
-        intent.putExtra(Intent.EXTRA_SUBJECT, CapUrl +" meme");
-        startActivity(Intent.createChooser(intent, "Share Using"));
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+//        intent.setType("text/plain");
+//        intent.putExtra(Intent.EXTRA_TEXT, ImgUrl);
+//        intent.putExtra(Intent.EXTRA_SUBJECT, CapUrl +" meme");
+//        startActivity(Intent.createChooser(intent, "Share Using"));
 
+        Bitmap bitmap = null;
+        
+        Drawable drawable = meme_image.getDrawable();
+        if(drawable instanceof BitmapDrawable){
+            bitmap = ((BitmapDrawable) meme_image.getDrawable()).getBitmap();
+        }
+        else {
+            return;
+        }
+        Uri bmpUri = null;
+
+        try {
+
+            File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                    "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+
+            bmpUri = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", file);
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, CapUrl +" meme");
+            shareIntent.setType("image/*");
+            shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+
+        } catch (Exception e){
+
+        }
     }
+
+//    public static void fixMediaDir() {
+//        File sdcard = Environment.getExternalStorageDirectory();
+//        if (sdcard == null) { return; }
+//        File dcim = new File(sdcard, "DCIM");
+//        if (dcim == null) { return; }
+//        File camera = new File(dcim, "Camera");
+//        if (camera.exists()) { return; }
+//        camera.mkdir();
+//    }
+
 }
